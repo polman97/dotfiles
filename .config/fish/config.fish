b2f50b56ee26951b1852b0e1d11cbca417a45473
Gijs Polman <polmangijs@hotmail.com>
2026-04-28 21:43:54 +0200

    removed greeting from kitty and added transparency

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrite greeting
-# potentially disabling fastfetch
-#function fish_greeting
-#    # smth smth
-#end
-
+# overwrites greeting disabling fastfetch
+function fish_greeting
+end
 alias vi vim
