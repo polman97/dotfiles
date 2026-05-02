4a44becc6b4351f2263f49efd5294d3d6b2a8858
Gijs Polman <polmangijs@hotmail.com>
2026-05-02 21:40:11 +0200

    Changed default terminal to ghostty and filebrowser to yazi

--- .bashrc
+++ .bashrc
@@ -2,9 +2,9 @@
 # ~/.bashrc
 #
 
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
 
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
-alias vi='vim'
-
 PS1='[\u@\h \W]\$ '
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-# overwrites greeting disabling fastfetch
-function fish_greeting
-end
-alias vi vim
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
